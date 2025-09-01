92ae24945617f15a81a04e21915bb8a7082e1ba9
wamalias <wardamalias@gmail.com>
2025-09-01 11:53:33 +0700

    feat: add get_caddy_done

---
+++ src/controllers/get_caddy_done.js
@@ -0,0 +1,70 @@
+import prisma from "../configs/prisma.js";
+
+// set tanggal hari ini
+const today = new Date();
+const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+// get caddy dengan status onfield pada hari ini
+const getCaddyOnField = async (req, res) => {
+  try {
+    const groups = await prisma.caddyGroup.findMany({
+      select: {
+        group_name: true,           // join tabel group dan caddy
+        caddies: {
+          where: {
+            onFields: {             // join tabel onfield
+              some: {
+                status: 1,          // filter status done
+                date_turun: {       // filter hanya yang tanggalnya hari ini
+                  gte: startOfDay,  // gte = greater than or equal
+                  lte: endOfDay,    // lt = less than
+                },
+              },
+            },
+          },
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    // Bentuk ulang JSON ke struktur yang diinginkan
+    const result = groups.map((g) => ({
+      group: {
+        nama: g.group_name,
+        caddies: g.caddies.map((c) => ({
+          id: c.id,
+          nama: c.name,
+        })),
+      },
+    }));
+
+    if (!result.length || result.every((g) => g.group.caddies.length === 0)) {
+      return res.status(404).json({
+        success: false,
+        message: "No caddy onfield found",
+        data: [],
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Caddy onfield retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.error("Error fetching caddy onfield:", error);
+
+    // Fallback error umum
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export default getCaddyOnField;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import getCaddyOnField from "../controllers/get_caddy_onfield.js";
 import getDetailOnField from "../controllers/get_detail_onfield.js";
-//import getCaddyDone from "../controllers/get_caddy_done.js";
+import getCaddyDone from "../controllers/get_caddy_done.js";
 
 const router = express.Router();
 
 router.get("/rekap/caddy_onfield", getCaddyOnField);
-//router.get("/rekap/caddy_done", getCaddyDone);
+router.get("/rekap/caddy_done", getCaddyDone);
 router.get("/rekap/detail_onfield/:id_caddy", getDetailOnField)
 
 export default router;
