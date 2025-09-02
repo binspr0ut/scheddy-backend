166d7eebc41ca9d66434c92e9ae60da575c4311b
wamalias <wardamalias@gmail.com>
2025-09-02 07:05:50 +0700

    fix port for deployment

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(RekapRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
