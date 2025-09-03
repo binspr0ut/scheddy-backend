b9fcc815477dd7497b303c3d0a331defc60ba7c3
binspr0ut <plattenecho@gmail.com>
2025-09-03 09:14:07 +0700

    feat: post-schedule completed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,6 +15,7 @@ import getCaddyBooking from "../controllers/get_caddy_booking.js";
 import getCaddyOnField from "../controllers/get_caddy_onfield.js";
 import getDetailOnField from "../controllers/get_detail_onfield.js";
 import getCaddyDone from "../controllers/get_caddy_done.js";
+import postSchedule from "../controllers/post_schedule.js";
 
 const router = express.Router();
 
@@ -34,5 +35,7 @@ router.put("/rekap/update/:id", putUpdateDone);
 router.get("/rekap/caddy_onfield", getCaddyOnField);
 router.get("/rekap/caddy_done", getCaddyDone);
 router.get("/rekap/detail_onfield/:id_caddy", getDetailOnField);
+
+router.post("/schedule/create", postSchedule);
 
 export default router;
